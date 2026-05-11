c943f71c5341f7ea2f28480b9e06fa77e2968968
radded <mycooljames@hotmail.com>
2026-05-11 13:59:37 -0400

    Add version numbering

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -10,6 +10,9 @@ export default function Footer() {
                 <Typography variant="caption">
                     Coded with occasional vibes by Radded.
                 </Typography>
+                <Typography variant="caption" sx={{marginLeft: "1rem"}}>
+                    Version: 0.426_alpha_webui_update
+                </Typography>
             </Box>
         </>
     );
