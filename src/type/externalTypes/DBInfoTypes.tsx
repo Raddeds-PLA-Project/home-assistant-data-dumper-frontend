a0d1bd81a750fec69e7b8d2f3877e593a4d5d34a
radded <mycooljames@hotmail.com>
2026-06-23 18:36:33 -0400

    update to account for locked database

--- src/type/externalTypes/DBInfoTypes.tsx
+++ src/type/externalTypes/DBInfoTypes.tsx
@@ -1,5 +1,5 @@
 export type DBInfoAPIResponse = {
-    entry_count: number,
+    entry_count: string, // Could be undefined if the database is locked, and we have no reason rn to serlialize the number
     is_unlocked: boolean,
-    newest_entry_time: string
+    newest_entry_time: string // Could be something other than time if database is locked or there are no entries
 };
